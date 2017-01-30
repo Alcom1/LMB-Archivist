8cda6f5f3ef6d00f93082ab3dd2d9c9a7ebb0269
Alexander Bronner <acbronn@gmail.com>
2017-01-29 19:28:29 -0500

    Hide unused options for beta.

--- LMB Archivist Formed/Form1.Designer.cs
+++ LMB Archivist Formed/Form1.Designer.cs
@@ -61,7 +61,7 @@
             this.button_archive.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.button_archive.Enabled = false;
             this.button_archive.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_archive.Location = new System.Drawing.Point(130, 187);
+            this.button_archive.Location = new System.Drawing.Point(130, 163);
             this.button_archive.Name = "button_archive";
             this.button_archive.Size = new System.Drawing.Size(243, 48);
             this.button_archive.TabIndex = 0;
@@ -81,7 +81,7 @@
             this.textBoxBottom.Name = "textBoxBottom";
             this.textBoxBottom.ReadOnly = true;
             this.textBoxBottom.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBoxBottom.Size = new System.Drawing.Size(477, 114);
+            this.textBoxBottom.Size = new System.Drawing.Size(477, 122);
             this.textBoxBottom.TabIndex = 5;
             // 
             // textBoxTop
@@ -95,7 +95,7 @@
             this.textBoxTop.Name = "textBoxTop";
             this.textBoxTop.ReadOnly = true;
             this.textBoxTop.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBoxTop.Size = new System.Drawing.Size(477, 110);
+            this.textBoxTop.Size = new System.Drawing.Size(477, 122);
             this.textBoxTop.TabIndex = 8;
             // 
             // archive_post_panel
@@ -112,7 +112,7 @@
             this.archive_post_panel.Enabled = false;
             this.archive_post_panel.Location = new System.Drawing.Point(13, 38);
             this.archive_post_panel.Name = "archive_post_panel";
-            this.archive_post_panel.Size = new System.Drawing.Size(485, 55);
+            this.archive_post_panel.Size = new System.Drawing.Size(485, 30);
             this.archive_post_panel.TabIndex = 10;
             // 
             // archive_radio_topics
@@ -164,7 +164,7 @@
             this.tooltip.BackColor = System.Drawing.SystemColors.Control;
             this.tooltip.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.tooltip.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tooltip.Location = new System.Drawing.Point(144, 7);
+            this.tooltip.Location = new System.Drawing.Point(144, 6);
             this.tooltip.Name = "tooltip";
             this.tooltip.Size = new System.Drawing.Size(16, 15);
             this.tooltip.TabIndex = 17;
@@ -178,7 +178,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 8);
+            this.label1.Location = new System.Drawing.Point(3, 7);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(58, 13);
             this.label1.TabIndex = 16;
@@ -186,7 +186,7 @@
             // 
             // textBoxUserId
             // 
-            this.textBoxUserId.Location = new System.Drawing.Point(64, 5);
+            this.textBoxUserId.Location = new System.Drawing.Point(64, 4);
             this.textBoxUserId.Name = "textBoxUserId";
             this.textBoxUserId.Size = new System.Drawing.Size(74, 20);
             this.textBoxUserId.TabIndex = 15;
@@ -201,7 +201,7 @@
             this.archive_topic_panel.Controls.Add(this.label3);
             this.archive_topic_panel.Controls.Add(this.textBoxUrl);
             this.archive_topic_panel.Enabled = false;
-            this.archive_topic_panel.Location = new System.Drawing.Point(12, 138);
+            this.archive_topic_panel.Location = new System.Drawing.Point(12, 116);
             this.archive_topic_panel.Name = "archive_topic_panel";
             this.archive_topic_panel.Size = new System.Drawing.Size(486, 30);
             this.archive_topic_panel.TabIndex = 11;
@@ -257,7 +257,7 @@
             // 
             this.archive_topic_radio.AutoSize = true;
             this.archive_topic_radio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.archive_topic_radio.Location = new System.Drawing.Point(16, 111);
+            this.archive_topic_radio.Location = new System.Drawing.Point(16, 89);
             this.archive_topic_radio.Name = "archive_topic_radio";
             this.archive_topic_radio.Size = new System.Drawing.Size(122, 20);
             this.archive_topic_radio.TabIndex = 14;
@@ -279,7 +279,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.splitContainer1.Location = new System.Drawing.Point(13, 254);
+            this.splitContainer1.Location = new System.Drawing.Point(13, 229);
             this.splitContainer1.Name = "splitContainer1";
             this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
@@ -290,8 +290,8 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.textBoxBottom);
-            this.splitContainer1.Size = new System.Drawing.Size(485, 244);
-            this.splitContainer1.SplitterDistance = 118;
+            this.splitContainer1.Size = new System.Drawing.Size(485, 269);
+            this.splitContainer1.SplitterDistance = 130;
             this.splitContainer1.SplitterWidth = 9;
             this.splitContainer1.TabIndex = 15;
             // 
