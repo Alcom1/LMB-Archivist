dc357fc60fbf2affb86ca2b205a8c2eeee5f4f31
Alexander Bronner <acbronn@gmail.com>
2017-02-26 09:55:08 -0500

    Jammin'

---
+++ LMB Archivist Formed/output/css/jammer.js
@@ -0,0 +1,27 @@
+var jammers = document.getElementsByClassName("lia-spoiler-link");
+var targets = document.getElementsByClassName("lia-spoiler-content");
+
+var jamToggle = function(subJammer, subTarget) {
+    var classListJammer = subJammer.classList;
+    
+    if(classListJammer.contains("open")) {
+        classListJammer.remove("open");
+        subTarget.style.cssText = '';
+    }
+    else {
+        classListJammer.add("open");
+        subTarget.style.cssText = 'display:block;';
+    }
+}
+
+for(var i = 0; i < jammers.length; i++) {
+    var jammer = jammers[i];
+    jammer.onclick = (function() {
+        var subJammer = jammers[i];
+        var subTarget = targets[i];
+
+        return function() { 
+            jamToggle(subJammer, subTarget);
+        }
+    })();
+}
